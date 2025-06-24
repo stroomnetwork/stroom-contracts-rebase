30a4cb0e8ec92d919424929a1cd6542147dd8abb
AleksDemian <demonnvel@gmail.com>
2025-06-24 10:54:30 +0300

    refactor generate set seed script

--- script/GenerateSetSeed.s.sol
+++ script/GenerateSetSeed.s.sol
@@ -20,56 +20,59 @@ contract GenerateSetSeedCalldataScript is Script {
     bytes32 public jointPublicKey = vm.envBytes32("JOINT_PUBLIC_KEY");
     uint8 public network = uint8(vm.envUint("BITCOIN_NETWORK"));
 
+
     function run() public view {
         console.log("=== GENERATION OF CALLDATA FOR SETSEED OPERATIONS ===\n");
 
-        _generateSetSeedCalldata();
+        bytes memory setSeedCalldata = abi.encodeWithSelector(
+            BTCDepositAddressDeriver.setSeed.selector, 
+            taprootAddress1, 
+            taprootAddress2, 
+            BitcoinNetworkEncoder.Network(network)
+        );
+
+        _printCalldataInfo(
+            "setSeed(string,string,uint8)",
+            userActivator,
+            setSeedCalldata,
+            keccak256("SetSeed2025"),
+            string.concat(
+                "\n  taprootAddress1: ", taprootAddress1, "\n",
+                "  taprootAddress2: ", taprootAddress2, "\n",
+                "  network: ", vm.toString(network), "\n"
+            )
+        );
 
         console.log("\n=== GENERATION OF CALLDATA FOR SETJOINTKEY OPERATIONS ===\n");
 
-        _generateSetJointKeyCalldata();
-    }
-
-    function _generateSetSeedCalldata() internal view {
-        BitcoinNetworkEncoder.Network _network = BitcoinNetworkEncoder.Network(network);
-
-        bytes memory functionCalldata = abi.encodeWithSelector(
-            BTCDepositAddressDeriver.setSeed.selector, taprootAddress1, taprootAddress2, _network
+        bytes memory setJointKeyCalldata = abi.encodeWithSelector(
+            ValidatorRegistry.setJointPublicKey.selector, 
+            jointPublicKey
         );
 
-        bytes32 salt = keccak256("SetSeed2025");
-
-        bytes32 operationId = keccak256(abi.encode(userActivator, 0, functionCalldata, bytes32(0), salt));
-
-        console.log("Function: setSeed(string,string,uint8)");
-        console.log("Params:");
-        console.log("  taprootAddress1:", taprootAddress1);
-        console.log("  taprootAddress2:", taprootAddress2);
-        console.log("  network:", network);
-        console.log("");
-        console.log("Target:", userActivator);
-        console.log("Value: 0");
-        console.log("Data:", vm.toString(functionCalldata));
-        console.log("Predecessor: 0x0000000000000000000000000000000000000000000000000000000000000000");
-        console.log("Salt:", vm.toString(salt));
-        console.log("Delay:", delay);
-        console.log("");
-        console.log("Operation ID:", vm.toString(operationId));
+        _printCalldataInfo(
+            "setJointPublicKey(bytes32)",
+            validatorRegistry,
+            setJointKeyCalldata,
+            keccak256("SetJointKey2025"),
+            string.concat("  jointPublicKey: ", vm.toString(jointPublicKey))
+        );
     }
 
-    function _generateSetJointKeyCalldata() internal view {
-        bytes memory functionCalldata =
-            abi.encodeWithSelector(ValidatorRegistry.setJointPublicKey.selector, jointPublicKey);
+    function _printCalldataInfo(
+        string memory functionSignature,
+        address target,
+        bytes memory functionCalldata,
+        bytes32 salt,
+        string memory params
+    ) internal view {
+        bytes32 operationId = keccak256(abi.encode(target, 0, functionCalldata, bytes32(0), salt));
 
-        bytes32 salt = keccak256("SetJointKey2025");
-
-        bytes32 operationId = keccak256(abi.encode(validatorRegistry, 0, functionCalldata, bytes32(0), salt));
-
-        console.log("Function: setJointPublicKey(bytes32)");
+        console.log("Function:", functionSignature);
         console.log("Params:");
-        console.log("  jointPublicKey:", vm.toString(jointPublicKey));
+        console.log(params);
         console.log("");
-        console.log("Target:", validatorRegistry);
+        console.log("Target:", target);
         console.log("Value: 0");
         console.log("Data:", vm.toString(functionCalldata));
         console.log("Predecessor: 0x0000000000000000000000000000000000000000000000000000000000000000");
