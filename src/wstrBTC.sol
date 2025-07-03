9a6aac1227ea74604aa6d22c1f4d5be1083d6464
AleksDemian <81850543+AleksDemian@users.noreply.github.com>
2025-07-03 11:09:12 +0300

    changed the implementation of the wrapper to erc4626 (#35)

--- src/wstrBTC.sol
+++ src/wstrBTC.sol
@@ -1,91 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
-import "../src/IStrBTC.sol";
 
-contract wstrBTC is ERC20Permit {
-    IStrBTC public strBTC;
-
-    error CannotWrapZero();
-    error CannotUnwrapZero();
-    error NoWstrBTCSupply();
-    error NoStrBTCBalance();
+contract wstrBTC is ERC4626, ERC20Permit {
+    /**
+     * @param _strBTC address of the strBTC token (underlying asset)
+     */
+    constructor(address _strBTC)
+        ERC4626(IERC20(_strBTC))
+        ERC20("Wrapped Stroom Bitcoin", "wstrBTC")
+        ERC20Permit("Wrapped Stroom Bitcoin")
+    {}
 
     /**
-     * @param _strBTC address of the strBTC token to wrap
+     * @notice Override decimals to match underlying asset (strBTC has 8 decimals)
      */
-    constructor(address _strBTC) ERC20("Wrapped Stroom Bitcoin", "wstrBTC") ERC20Permit("Wrapped Stroom Bitcoin") {
-        strBTC = IStrBTC(_strBTC);
+    function decimals() public view override(ERC4626, ERC20) returns (uint8) {
+        return ERC4626.decimals();
     }
 
     /**
-     * @notice Wraps strBTC into wstrBTC.
-     * @param strBTCAmount Amount of strBTC to wrap.
-     * @return Amount of wstrBTC minted.
+     * @notice Returns the total amount of underlying strBTC held by the vault
      */
-    function wrap(uint256 strBTCAmount) external returns (uint256) {
-        if (strBTCAmount == 0) revert CannotWrapZero();
-
-        uint256 strBTCBalance = strBTC.balanceOf(address(this));
-        uint256 wstrBTCSupply = totalSupply();
-
-        uint256 wstrBTCAmount;
-        if (wstrBTCSupply == 0 || strBTCBalance == 0) {
-            // Initial wrap: 1:1 ratio
-            wstrBTCAmount = strBTCAmount;
-        } else {
-            wstrBTCAmount = (strBTCAmount * wstrBTCSupply) / strBTCBalance;
-        }
-
-        strBTC.transferFrom(msg.sender, address(this), strBTCAmount);
-        _mint(msg.sender, wstrBTCAmount);
-
-        return wstrBTCAmount;
-    }
-
-    /**
-     * @notice Unwraps wstrBTC back into strBTC.
-     * @param wstrBTCAmount Amount of wstrBTC to unwrap.
-     * @return Amount of strBTC returned.
-     */
-    function unwrap(uint256 wstrBTCAmount) external returns (uint256) {
-        if (wstrBTCAmount == 0) revert CannotUnwrapZero();
-
-        uint256 strBTCBalance = strBTC.balanceOf(address(this));
-        uint256 wstrBTCSupply = totalSupply();
-
-        if (wstrBTCSupply == 0) revert NoWstrBTCSupply();
-
-        uint256 strBTCAmount = (wstrBTCAmount * strBTCBalance) / wstrBTCSupply;
-
-        _burn(msg.sender, wstrBTCAmount);
-        strBTC.transfer(msg.sender, strBTCAmount);
-
-        return strBTCAmount;
-    }
-
-    /**
-     * @notice Get the current amount of strBTC for amount of wstrBTC.
-     * @return Amount of strBTC per amount of wstrBTC.
-     */
-    function strBTCPerToken(uint256 amount) external view returns (uint256) {
-        uint256 strBTCBalance = strBTC.balanceOf(address(this));
-        uint256 wstrBTCSupply = totalSupply();
-        if (wstrBTCSupply == 0) revert NoWstrBTCSupply();
-
-        return (strBTCBalance * amount) / wstrBTCSupply;
-    }
-
-    /**
-     * @notice Get the current amount of wstrBTC for amount of strBTC.
-     * @return Amount of wstrBTC per amount of strBTC.
-     */
-    function tokensPerStrBTC(uint256 amount) external view returns (uint256) {
-        uint256 strBTCBalance = strBTC.balanceOf(address(this));
-        uint256 wstrBTCSupply = totalSupply();
-        if (strBTCBalance == 0) revert NoStrBTCBalance();
-
-        return (wstrBTCSupply * amount) / strBTCBalance;
+    function totalAssets() public view override returns (uint256) {
+        return IERC20(asset()).balanceOf(address(this));
     }
 }
